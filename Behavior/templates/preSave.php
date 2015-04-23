fa21598a57895904677d4bb9e58bc56ff06bd4b7
Yaroslav Gashuk <fenrizbes@yandex.ru>
2015-04-23 18:00:42 +0500

    Force deletion ability

--- Behavior/templates/preSave.php
+++ Behavior/templates/preSave.php
@@ -2,7 +2,7 @@
 if ($this->isColumnModified('<?php echo $column; ?>')) {
     if ($this-><?php echo $getter; ?>()) {
         $this-><?php echo $setter; ?>($this->moveUploadedFile($this-><?php echo $getter; ?>()));
-    } else {
+    } elseif ($this-><?php echo $getter; ?>() !== false) {
         $this->resetModified('<?php echo $column; ?>');
     }
 }
