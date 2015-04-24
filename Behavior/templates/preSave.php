31ee4c95000139789993c3f00783cac6c0053678
Yaroslav Gashuk <fenrizbes@yandex.ru>
2015-04-24 14:34:08 +0500

    Fix null value

--- Behavior/templates/preSave.php
+++ Behavior/templates/preSave.php
@@ -1,10 +1,4 @@
 
 if ($this->isColumnModified('<?php echo $column; ?>')) {
-    if (is_null($this-><?php echo $getter; ?>())) {
-        $this->resetModified('<?php echo $column; ?>');
-    } elseif ($this-><?php echo $getter; ?>()) {
-        $this-><?php echo $setter; ?>($this->moveUploadedFile($this-><?php echo $getter; ?>()));
-    } else {
-        $this-><?php echo $setter; ?>(null);
-    }
+    $this-><?php echo $setter; ?>($this->moveUploadedFile($this-><?php echo $getter; ?>()));
 }
